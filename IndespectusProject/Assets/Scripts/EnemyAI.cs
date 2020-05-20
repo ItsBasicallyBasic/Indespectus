e5dbd7958524267c435fb6bf461ae62212aafa2a
Alex <alexnorth1403@gmail.com>
2020-05-20 15:21:43 +1000

    Fixing Downgrade Conflicting Files
    After the downgrade unity had conflicting files so this should resolve those.
    
    Also AI and velocity components have been added and some scene changes.

---
+++ IndespectusProject/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    // AI ESM Enumerator
+    public enum EnemyBehaviours
+    {
+        Idle,
+        Attacking,
+        Fleeing,
+        Scouting
+    }
+
+    public EnemyBehaviours enemyBehaviour;
+
+    // Nav Mesh Agent
+    private NavMeshAgent enemyNavMeshAgent;
+
+    // Target GameObject
+    public GameObject player;
+
+    // Target Location
+    public Vector3 target;
+
+    // Player's last known location
+    private Vector3 playerLastKnownLocation;
+
+    // Enemy scout variables
+    public Transform scoutLocationsParent;
+    public List<Transform> scoutLocations;
+    public Transform nextLocation;
+    public float locationTimer = 10;
+    private float locationTime;
+
+    // Enemy attack range
+    public float attackDistance = 1;
+
+    // Enemy run range
+    public float runDistance = 5;
+
+    // Enemy movement speeds
+    public float runSpeed = 5;
+    public float stealthSpeed = 1;
+
+    // Rotation Variables
+    private float rotationSpeed = 5.0f;
+    private float adjRotSpeed;
+    private Quaternion targetRotation;
+
+    // Animation variables
+    public bool attacking = false;
+
+    // Player velocity variables
+    private float playerVelocity;
+
+    // Rigidbody
+    public Rigidbody rb;
+
+    // Animation variables
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBehaviour = EnemyBehaviours.Scouting;
+
+        // Get nav mesh agent from gameobject
+        enemyNavMeshAgent = GetComponent<NavMeshAgent>();
+
+        // Add each scout point to scoutLocations list
+        foreach(Transform child in scoutLocationsParent)
+        {
+            scoutLocations.Add(child);
+        }
+
+        // Assign first scout location
+        int randomIndex = Random.Range(0, scoutLocations.Count);
+        nextLocation = scoutLocations[randomIndex];
+
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerVelocity = player.GetComponent<PlayerVelocity>().velocity;
+
+        if (!attacking)
+        {
+            // Look at target
+            RotateTowardsTarget(new Vector3(target.x, transform.position.y, target.z));
+        }
+
+        if (playerVelocity > 0.5f)
+        {
+            playerLastKnownLocation = player.GetComponent<PlayerVelocity>().head.transform.position;
+            enemyBehaviour = EnemyBehaviours.Attacking;
+        }
+
+        // Enemy Behaviours State Switch
+        switch (enemyBehaviour)
+        {
+            case EnemyBehaviours.Scouting:
+                Scouting();
+                break;
+            case EnemyBehaviours.Attacking:
+                Attacking();
+                break;
+            case EnemyBehaviours.Fleeing:
+                Fleeing();
+                break;
+        }
+    }
+
+    void Scouting()
+    {
+
+        // Setting position to scout toward
+        target = nextLocation.position;
+
+        // Enable NavMeshAgent
+        enemyNavMeshAgent.enabled = true;
+
+        // Set NavMeshAgent Speed
+        enemyNavMeshAgent.speed = stealthSpeed;
+
+        // Scout location timer
+        if(Time.time > locationTime || Vector3.Distance(nextLocation.position, transform.position) < 1f)
+        {
+            locationTime = Time.time + locationTimer;
+
+            // Choose enemy scout location using random index
+            int randomIndex = Random.Range(0, scoutLocations.Count);
+            enemyNavMeshAgent.destination = scoutLocations[randomIndex].position;
+
+            nextLocation = scoutLocations[randomIndex];
+        }
+
+        // If player becomes visible attack
+    }
+
+    void Attacking()
+    {
+        // Setting target to attack
+        target = playerLastKnownLocation;
+
+        if (Vector3.Distance(transform.position, playerLastKnownLocation) <= attackDistance)
+        {
+            enemyNavMeshAgent.enabled = false;
+            GetComponent<PlayerVelocity>().velocity = 1;
+            
+            anim.SetTrigger("isAttacking");
+            StartCoroutine(attackWait());
+        }
+        else if(Vector3.Distance(transform.position, playerLastKnownLocation) > attackDistance && Vector3.Distance(transform.position, playerLastKnownLocation) > runDistance)
+        {
+            // Enable NavMeshAgent, set speed and set destination
+            enemyNavMeshAgent.enabled = true;
+            enemyNavMeshAgent.speed = stealthSpeed;
+            enemyNavMeshAgent.destination = playerLastKnownLocation;
+            //anim.SetTrigger("isWalking");
+        }
+        else if (Vector3.Distance(transform.position, playerLastKnownLocation) < runDistance)
+        {
+            // Enable NavMeshAgent, set speed and set destination
+            enemyNavMeshAgent.enabled = true;
+            enemyNavMeshAgent.speed = runSpeed;
+            enemyNavMeshAgent.destination = playerLastKnownLocation;
+            //anim.SetTrigger("isRunning");
+        }
+    }
+
+    void Fleeing()
+    {
+        enemyNavMeshAgent.enabled = false;
+        target = playerLastKnownLocation;
+        transform.Translate(Vector3.forward * -runSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(target, transform.position) > 5)
+        {
+            enemyBehaviour = EnemyBehaviours.Scouting;
+        }
+    }
+
+    void RotateTowardsTarget(Vector3 targetPos)
+    {
+        //Lerp Towards target
+        targetRotation = Quaternion.LookRotation(targetPos - transform.position);
+        adjRotSpeed = Mathf.Min(rotationSpeed * Time.deltaTime, 1);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, adjRotSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Sword")
+        {
+
+            enemyBehaviour = EnemyBehaviours.Idle;
+            enemyNavMeshAgent.enabled = false;
+            rb.isKinematic = false;
+        }
+    }
+
+    private IEnumerator attackWait()
+    {
+        yield return new WaitForSeconds(2);
+        anim.ResetTrigger("isAttacking");
+        enemyBehaviour = EnemyBehaviours.Fleeing;
+    }
+}
--- IndespectusProject/Assets/Scripts/PlayerVelocity.cs
+++ IndespectusProject/Assets/Scripts/PlayerVelocity.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+>>>>>>> Stashed changes
 
 public class PlayerVelocity : MonoBehaviour
 {
 
     public float velocity;
+<<<<<<< Updated upstream
+=======
+    public float desiredVelocity;
+>>>>>>> Stashed changes
 
     private float headVelocity;
     private float leftHandVelocity;
@@ -19,6 +28,14 @@ public class PlayerVelocity : MonoBehaviour
     public Transform rightHand;
     public Transform leftHand;
 
+<<<<<<< Updated upstream
+=======
+    public Material material;
+    public Material material2;
+
+    private float lerpSpeed = 0.1f;
+
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -26,15 +43,36 @@ public class PlayerVelocity : MonoBehaviour
         headPreviousPos = head.position;
         rightHandPreviousPos = rightHand.position;
         leftHandPreviousPos = leftHand.position;
+<<<<<<< Updated upstream
+=======
+
+        if (gameObject.tag != "Player")
+        {
+            //material = GetComponent<MeshRenderer>().material;
+        }
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         // Assign velocity based off previous frame position
         leftHandVelocity = Vector3.Distance(leftHand.position, leftHandPreviousPos) * Time.deltaTime;
         rightHandVelocity = Vector3.Distance(rightHand.position, rightHandPreviousPos) * Time.deltaTime;
         headVelocity = Vector3.Distance(head.position, headPreviousPos) * Time.deltaTime;
+=======
+        if(gameObject.tag != "Player")
+        {
+            material.SetFloat("_Transition", velocity);
+            material2.SetFloat("_Transition", velocity);
+        }
+
+        // Assign velocity based off previous frame position
+        leftHandVelocity = Vector3.Distance(leftHand.position, leftHandPreviousPos) * 5000 * Time.deltaTime;
+        rightHandVelocity = Vector3.Distance(rightHand.position, rightHandPreviousPos) * 5000 * Time.deltaTime;
+        headVelocity = Vector3.Distance(head.position, headPreviousPos) * 5000 * Time.deltaTime;
+>>>>>>> Stashed changes
 
         // Update previous frame with current frame position for next frame
         headPreviousPos = head.position;
@@ -44,6 +82,7 @@ public class PlayerVelocity : MonoBehaviour
         // Update player velocity with sensory output that was the highest
         if(leftHandVelocity > rightHandVelocity || leftHandVelocity > headVelocity)
         {
+<<<<<<< Updated upstream
             velocity = leftHandVelocity;
         }
         else if (rightHandVelocity > leftHandVelocity || rightHandVelocity > headVelocity)
@@ -53,11 +92,30 @@ public class PlayerVelocity : MonoBehaviour
         else if (headVelocity > rightHandVelocity || headVelocity > leftHandVelocity)
         {
             velocity = headVelocity;
+=======
+            desiredVelocity = leftHandVelocity;
+        }
+        else if (rightHandVelocity > leftHandVelocity || rightHandVelocity > headVelocity)
+        {
+            desiredVelocity = rightHandVelocity;
+        }
+        else if (headVelocity > rightHandVelocity || headVelocity > leftHandVelocity)
+        {
+            desiredVelocity = headVelocity;
+>>>>>>> Stashed changes
         }
         // Else in the unlikely case that all sensory inputs are moving at the same speed
         else
         {
+<<<<<<< Updated upstream
             velocity = headVelocity;
         }
+=======
+            desiredVelocity = headVelocity;
+        }
+
+        velocity = Mathf.Lerp(velocity, desiredVelocity, lerpSpeed);
+        if (desiredVelocity > 1) desiredVelocity = 1;
+>>>>>>> Stashed changes
     }
 }
