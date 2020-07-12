f6073a7b9fbdc70b511b59303541b5cd8656ba38
Tamar <tamarlouise.p@gmail.com>
2020-07-13 01:06:07 +1000

    networking wip

---
+++ IndespectusProject/Assets/Scripts/NetworkingScripts/LobbyScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyScript : MonoBehaviourPunCallbacks {
+    
+    public static LobbyScript lobby;
+    public GameObject connectButton;
+    public GameObject cancelButton;
+
+    void Awake() {
+        lobby = this;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("Player has connected to master server");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        connectButton.SetActive(true);
+    }
+
+    public void OnConnectButtonClicked() {
+        Debug.Log("Connecting...");
+        connectButton.SetActive(false);
+        PhotonNetwork.JoinRandomRoom();
+        cancelButton.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        Debug.Log("tried to join random room but failed");
+        CreateRoom();
+    }
+
+    void CreateRoom() {
+        Debug.Log("Creating new room...");
+        int rRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() {
+            IsVisible = true, 
+            IsOpen = true, 
+            MaxPlayers = (byte)MultiplayerSettings.multiplayerSettings.maxPlayers
+        };
+        PhotonNetwork.CreateRoom("Room" + rRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        Debug.Log("tried to create new room but failed");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked () {
+        Debug.Log("Cancelling...");
+        cancelButton.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+        connectButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
