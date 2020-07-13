e64c8a288fbd790694041b2a81bce76753066bc0
Fallere825-s <64529227+Fallere825-s@users.noreply.github.com>
2020-07-13 12:53:35 +0900

    Add files via upload
    there is a same file but i upload on the wrong place please detete them

---
+++ IndespectusProject/Assets/Scripts/SE.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SE : MonoBehaviour
+{
+    public AudioClip clip;
+
+    public float volume;
+    public float pitch;
+
+}
