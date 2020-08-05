43dc95f51c56f35438912f0465127857d6d93301
tam <tamarlouise.p@gmail.com>
2020-08-06 02:21:03 +1000

    added bunch of network transforms, seeing whats needed

--- IndespectusProject/Assets/Scripts/NetworkingScripts/MyCamera.cs
+++ IndespectusProject/Assets/Scripts/NetworkingScripts/MyCamera.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 
 public class MyCamera : MonoBehaviour
 {
-    [SerializeField] private Camera myCam;
+    [SerializeField] private GameObject myCam;
+    [SerializeField] private GameObject hudUI;
     [SerializeField] private PhotonView myPView;
     // Start is called before the first frame update
     void Start() {
         if(myPView == null) {myPView = this.gameObject.GetComponent<PhotonView>();}
         if(!myPView.IsMine) {
-            //myCam.enabled = false;
-            Destroy(myCam);
+            myCam.SetActive(false);
+            hudUI.SetActive(false);
+            // Destroy(myCam);
         }
     }
 
