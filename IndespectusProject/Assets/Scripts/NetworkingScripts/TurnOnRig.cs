d817f928c331e64b5eba6da9b94bca958207a364
tam <tamarlouise.p@gmail.com>
2020-08-12 17:06:18 +1000

    issue fix #2?

---
+++ IndespectusProject/Assets/Scripts/NetworkingScripts/TurnOnRig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class TurnOnRig : MonoBehaviour {
+    
+    [SerializeField] PhotonView PV;
+    [SerializeField] GameObject rig;
+
+    // Start is called before the first frame update
+    void Start() {
+        if(PV.IsMine)
+            rig.SetActive(true);
+    }
+}
