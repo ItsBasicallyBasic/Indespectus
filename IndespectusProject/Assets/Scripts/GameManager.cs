a14a3fb18f16f16177732c4d09182fade2c99ccd
Tam Phillips <tamarlouise.p@gmail.com>
2020-09-02 17:05:49 +1000

    added GameManager game object

---
+++ IndespectusProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    
+    public static GameManager GM = null;
+    
+    void Awake() {
+        // Singleton
+        if(GM == null) { 
+             GM = this;
+        } else if(GM != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
