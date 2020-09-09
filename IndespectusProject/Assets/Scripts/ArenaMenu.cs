eda1e0c2c19c2511643deef3a4d9d6fc43b84d79
Tam Phillips <tamarlouise.p@gmail.com>
2020-09-09 21:35:21 +1000

    Playtesting issues 1, 2, 3, 5, 6, 7, 11 & 13 & Performance Tweaks

---
+++ IndespectusProject/Assets/Scripts/ArenaMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArenaMenu : MonoBehaviourPunCallbacks {
+    
+    [SerializeField] GameObject arenaMenu;
+    [SerializeField] GameObject Hands;
+    [SerializeField] bool menuActive = false;
+    [SerializeField] bool handsActive = false;
+    Scene currentScene;
+    void Start() {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if((OVRInput.Get(OVRInput.RawButton.Start) || Input.GetKey(KeyCode.Escape)) && !menuActive) {
+            menuActive = true;
+            handsActive = true;
+        }
+        if((OVRInput.GetDown(OVRInput.Button.Two) || Input.GetKeyDown(KeyCode.Escape)) && menuActive) {
+            menuActive = false;
+            handsActive = false;
+        }
+        if((OVRInput.GetDown(OVRInput.Button.Four) || Input.GetKeyDown(KeyCode.Tilde)) && menuActive) {
+            Application.Quit();
+            PhotonNetwork.Disconnect();
+            SceneManager.LoadScene(2);
+        }
+        if(currentScene.buildIndex != 1){
+            handsActive = true;
+        } 
+        arenaMenu.SetActive(menuActive);
+        Hands.SetActive(handsActive);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        base.OnDisconnected(cause);
+        SceneManager.LoadScene(MultiplayerSettings.multiplayerSettings.endScene);
+    }
+
+}
